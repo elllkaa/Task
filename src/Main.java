57e6a564d062d36aca4fc4f881b3c072bd82f48b
Пользователь <d.elllka@bk.ru>
2023-04-08 09:03:03 +0300

    task

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
-
-
     public static void main(String[] args) {
         int[] arr = { 10, 20, 30, 40, 50 };
         reverse(arr, arr.length);
     }
-
     static void reverse(int[] a, int n)
     {
         int i;
@@ -16,7 +13,6 @@ public class Main {
             a[i] = a[n - i - 1];
             a[n - i - 1] = t;
         }
-
         System.out.println("Reversed array is: \n");
         for (b = 0; b < n; b++) {
             System.out.println(a[b]);
